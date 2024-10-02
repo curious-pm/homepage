df7173e1c278588f5d01fdf44e3715612e898e25
curious-pm <ankit@fastmail.com>
2024-10-02 12:24:42 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      './index.html',  // Adjust this path to match your HTML file location
+      './*.{js,ts,jsx,tsx}',  // Adjust this to match your file types
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
